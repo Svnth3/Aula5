9ffc3bd3f734c5c63cc4135ad9512f9186f0a9c5
Svnth3 <marinsfraga@gmail.com>
2025-04-17 21:36:14 -0300

    Adicionando exercicios

---
+++ Aula_4/atividade2.py
@@ -0,0 +1,20 @@
+# for n in range(1,11):
+#     print(n)
+
+# for m in range(5):
+#     print("Estudando Python")
+# for j in range(3):
+#     nome = input("Informe o Nome: ")
+#     print(nome)
+
+
+soma = 0.0
+for n in range(5):
+    cont = n+1
+    num = float(input("Digite um numero:"))
+    soma += num
+
+print(f'A soma total foi de {soma}')
+media = (soma/cont)
+print(f'A média é de {media}')
+
---
+++ Aula_4/exemplo_for.py
@@ -0,0 +1,23 @@
+#for n in range(5):
+    #print("olá mundo")
+#for n in range(5):
+    #print(n)
+# for i in range(5,20):
+    # print(i)
+# for j in range(5,50,5):
+    # print(j)
+# inicio = int(input("Informe o inicio da contagem: "))
+# fim = int(input("informe o final da contagem: "))
+# for u in range(inicio,fim):
+#     print(u)
+
+# for n in range(3):
+#     print(f'\npessoa {n+1}')
+    
+#     nome = input("Informe o nome: ")
+#     print(f'O nome da pessoa é: {nome}.')
+# soma = 0.
+# for num in range(5):
+#     numero = float(input("Digite um número: "))
+#     soma += numero #soma = soma + numero.
+# print(f'O total é de {soma}')
